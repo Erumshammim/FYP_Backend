f62b39258b01ec15c1c8f5951c9ea800281fbe4c
ifrabatool123 <ifrabatool277@gmail.com>
2021-09-03 16:00:59 +0500

    Updated User Authentication

--- fypbackend/accounts/serializers.py
+++ fypbackend/accounts/serializers.py
@@ -8,15 +8,32 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email')
 
+
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
+    is_admin = serializers.BooleanField(required=False)
+    is_data_entry = serializers.BooleanField(required=False)
+    confirm_password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'email', 'password', 'confirm_password',
+                    'is_admin', 'is_data_entry')
+        extra_kwargs = {'password': {'write_only': True, 'style': {'input_type': 'password'}}}
 
     def create(self, validated_data):
+        password = validated_data['password']
+        confirm_password = validated_data['confirm_password']
+
+        if password != confirm_password:
+            raise serializers.ValidationError({'password': 'Password must match.'})
+        
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        
+        if validated_data['is_admin'] == True:
+            user.is_superuser = True
+        if validated_data['is_data_entry'] == True:
+            user.is_staff = True
+        user.save()
 
         return user
 
@@ -24,7 +41,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
-    fields = ('id', 'username', 'email')
+    fields = ('id', 'username', 'email', 'is_superuser', 'is_staff')
 
 # Change Password
 from rest_framework import serializers
